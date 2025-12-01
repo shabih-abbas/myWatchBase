49bc407ad7c6bb2e1d8f3a62b048f95ec151db68
shabihabbas09 <shabihabbas09@gmail.com>
2025-12-01 17:02:26 +0000

    footer added

--- client/src/components/MainLayout/MainLayout.jsx
+++ client/src/components/MainLayout/MainLayout.jsx
@@ -1,5 +1,6 @@
 import {Outlet, Link} from 'react-router'
 import logo from '../../assets/dark.svg';
+import tmdb from '../../assets/tmdb.png';
 import styles from './MainLayout.module.css';
 import Menu from '../Menu/Menu';
 
@@ -13,6 +14,23 @@ export default function MainLayout(){
                 </nav>  
             </header>  
             <Outlet/>
+            <footer className={styles.footer}>
+                <div className={styles.footerBrand}>
+                    <img src={logo} alt="logo" className={styles.footerLogo}/>
+                    <address><em>Made with ❤️ by </em><a target="_blank" href="mailto:shabihabbas09@gmail.com">Shabih Abbas</a></address>
+                    <p>© 2025</p>    
+                
+                    <div className={styles.footerLinks}>
+                        <a target="_blank" href="https://github.com/shabih-abbas/myWatchBase">Github</a>
+                        <a target="_blank" href="https://www.linkedin.com/in/shabih-abbas-826b35232/">Linkdin</a>
+                    </div>
+                </div>
+                <div className={styles.attributions}>
+                    <p className={styles.tmdbText}><img className={styles.tmdb} src={tmdb} alt="tmdb logo" /> <span>This product uses the TMDB API but is not endorsed or certified by TMDB.</span> </p>
+                    <p>Data from OMDb</p>
+                    <p>This is a demonstration project. No personal data is sold or shared. Cookies are used strictly for authentication purposes.</p>
+                </div>
+            </footer>
         </>
     )
 }
--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -1,19 +1,72 @@
-import { Link } from 'react-router';
+import {useState} from 'react';
+import { Link, useNavigate } from 'react-router';
 import { MdOutlineExplore } from "react-icons/md";
+import { TiThMenu } from "react-icons/ti";
+import { useAuth } from '../Providers/AuthProvider';
+import { useLoading } from '../Providers/LoadingProvider';
+import { useError } from '../Providers/ErrorProvider';
 import ProfileIcon from '../ProfileIcon/ProfileIcon';
 import SearchBar from '../SearchBar/SearchBar';
 import Tooltip from '../Tooltip/Tooltip';
 import styles from './Menu.module.css';
 export default function Menu(){
+    const [show, setShow] = useState(false);
+    const {user, setUser} = useAuth();
+    const {setLoading} = useLoading();
+    const {setError} = useError();
+    const Navigate = useNavigate();
     return (
         <menu className={styles.menu}>
             <SearchBar />
-            <Tooltip label="Explore Movies">
-                <Link to='/discover' className={styles.explore}>
-                    <MdOutlineExplore aria-label="explore" className={styles.icon} />
-                </Link>
-            </Tooltip>
-            <ProfileIcon />
+            <div className={styles.lg}>
+                <Tooltip label="Discover Movies">
+                    <Link to='/discover' className={styles.explore}>
+                        <MdOutlineExplore aria-label="explore" className={styles.icon} />
+                    </Link>
+                </Tooltip>
+                <ProfileIcon />
+            </div>
+            <div className={styles.sm}>
+                <TiThMenu onClick={() => setShow((prev) => !prev)} className={styles.icon} />
+                {show ? (
+                    <div className={styles.dropdown}>
+                        <Link to='/discover' className={styles.link}>Discover</Link>
+                        {user ? (
+                            <>
+                                <Link to='/collections' className={styles.link}>Collections</Link>
+                                <button onClick={logout} className={styles.logout}>Logout</button>
+                            </>
+                        ): <Link to='/login' className={styles.link}>Login</Link>}
+                        
+
+
+                    </div>
+                ): null}
+            </div>
+            
         </menu>
     )
+    async function logout() {
+        setError(null);
+        setLoading(true);
+        try {
+          const res = await fetch("/api/auth/logout", {
+            method: "POST",
+            credentials: "include",
+          });
+          if (res.ok) {
+            setUser(null);
+            Navigate("/");
+          } else {
+            const data = await res.json();
+            setError(data.message);
+          }
+        } catch (err) {
+          setError("Could not perform logout, please try later.");
+          console.error("Logout Error", err);
+        } finally {
+          setLoading(false);
+          setShow(false);
+        }
+      }
 }
